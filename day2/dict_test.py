dc5c784be8cb87c0029fce78bb43538ade927009
anand84471 <35921176+anand84471@users.noreply.github.com>
2021-12-16 20:25:09 +0530

    commit changes

---
+++ day2/dict_test.py
@@ -0,0 +1,8 @@
+anand={
+    "name":"Anand",
+    "email":"anand@gmail.com",
+    "phone_no":"1234567890",
+    "address":"Noida"
+}
+print(anand.get("first_name","not found"))
+print(anand["name"])
